63b7f1f8c2c601a27e869f69a7f0e012eb61e9e1
Vinit Joshi <vinitjoshi1989@gmail.com>
2026-07-24 16:12:39 +0300

    Configure frontend for environment-based API endpoint

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,11 +1,12 @@
 import axios from "axios";
-
+console.log("VITE_API_BASE_URL =", import.meta.env.VITE_API_BASE_URL);
+console.log("MODE =", import.meta.env.MODE);
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
   headers: {
     "Content-Type": "application/json",
   },
-  timeout: 10000,
+  timeout: 30000,
 });
 
 export default api;
